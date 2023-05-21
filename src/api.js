85b88a284eadb6b281fc990d953ca1ef291ccce7
vaibhav9849 <134196260+vaibhav9849@users.noreply.github.com>
2023-05-21 14:31:52 +0530

    this is a react app for weather data and forecast developed by using openweatherapi and geodbcities

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export  const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "9f89325d96msh623772a7a74b85dp15c1b1jsn6e283012f6ca",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com"
+    }
+  };
+export const GEO_API_URL="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="c86ec20d86898ec488a658352b743c96";
+
